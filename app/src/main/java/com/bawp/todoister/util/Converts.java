3607b2c6041be79276bf4ba7fd5f9276d87e89d5
phhduc <55909104+phhduc@users.noreply.github.com>
2021-12-28 19:15:12 +0700

    init

---
+++ app/src/main/java/com/bawp/todoister/util/Converts.java
@@ -0,0 +1,27 @@
+package com.bawp.todoister.util;
+
+import androidx.room.TypeConverter;
+
+import com.bawp.todoister.model.Priority;
+
+import java.util.Date;
+
+public class Converts {
+    @TypeConverter
+    public static Date fromTimestamp(Long value){
+        return value == null ? null: new Date(value);
+    }
+    @TypeConverter
+    public static Long dateToTimestamp(Date date){
+        return date == null ? null: date.getTime();
+    }
+    @TypeConverter
+    public static String formPriority(Priority priority){
+        return priority == null ? null : priority.name();
+    }
+    @TypeConverter
+    public static Priority toPriority(String priority){
+        return priority == null ? null : Priority.valueOf(priority);
+    }
+
+}
